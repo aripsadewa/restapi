5d4867f1148e046f14c1bf32b1f9acc431e5969b
findarip <find.arip@gmail.com>
2021-09-21 20:02:39 +0700

    tambah tampil semua mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("aplikasi berjalan",res)
 };
+
+//tampil semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('select * from mahasiswa', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
