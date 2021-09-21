5d36eac7789ef2f4d989ffba742000a626656274
findarip <find.arip@gmail.com>
2021-09-21 21:16:10 +0700

    tampil by id

--- controller.js
+++ controller.js
@@ -9,11 +9,23 @@ exports.index = function(req,res){
 
 //tampil semua data mahasiswa
 exports.tampilsemuamahasiswa = function(req,res){
-    connection.query('select * from mahasiswa', function(error, rows, fileds){
+    connection.query('select * from mahasiswa', function(error, rows, fields){
         if(error){
             connection.log(error);
         } else {
             response.ok(rows, res)
+        }
+    });
+};
+
+exports.tampilbyid = function(req,res){
+    let id = req.params.id;
+    connection.query('select * from mahasiswa where id = ?',  [id],
+        function(error, rows, fields){
+            if(error){
+                connection.log(error);
+            } else {
+                response.ok(rows, res)
         }
     });
 };
