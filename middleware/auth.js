61938c236546001e7d54f2eb1a213a3f09bbf02a
findarip <find.arip@gmail.com>
2021-09-23 22:11:03 +0700

    login aman

--- middleware/auth.js
+++ middleware/auth.js
@@ -36,9 +36,62 @@ exports.registrasi = function(req, res) {
                         response.ok("Berhasil tambah user", res);
                     }
                 });
-            } else {
+            }else {
                 response.ok("Email sudah terdaftar", res);
             }
         }
     })
+}
+
+exports.login = function(req, res){
+    var post = {
+        password: req.body.password,
+        email: req.body.email
+    }
+
+    var query = "select * from ?? where ??=? and ??=?";
+    var table = ["user", "password", md5(post.password), "email", post.email];
+
+    query = mysql.format(query,table);
+
+    connection.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        }else {
+            if(rows.length == 1){
+                // res.json({"error": true, "Message":"kondisi masuk"});
+
+                var token = jwt.sign({rows}, config.secret, {
+                    expiresIn: 1440
+                });
+
+                id_user = rows[0].id;
+
+                var data = {
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                var query = "insert into ?? set ?";
+                var table = ["akses_token"];
+
+                query = mysql.format(query, table);
+                connection.query(query, data, function(error, rows){
+                    if(error){
+                        console.log(error);
+                    } else {
+                        res.json({
+                            success: true,
+                            message: 'TOken JWT Tergenerate',
+                            token: token,
+                            currUser: data.id_user
+                        });
+                    }
+                });
+            }else {
+                 res.json({"error": true, "Message":"Email atau password salah"});
+            }
+        }
+    });
 }
